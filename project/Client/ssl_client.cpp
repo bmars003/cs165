bfd56c7418c244b5de7ecd5f62cd2f310f0d59f4
Bryan <cse.program.developer@gmail.com>
2012-12-07 22:01:45 -0800

    Cleaning up. Server able to sent file requested contents. Client able to receive from client and display, but no actial file is saved yet.

---
+++ project/Client/ssl_client.cpp
@@ -0,0 +1,314 @@
+//----------------------------------------------------------------------------
+// File: ssl_client.cpp
+// Description: Implementation of an SSL-secured client that performs
+//              secure file transfer with a single server over a single
+//              connection
+//----------------------------------------------------------------------------
+#include <string>
+#include <time.h>               // to seed random number generator
+#include <sstream>          // stringstreams
+//added
+#include <iostream>
+#include <openssl/rand.h>
+
+using namespace std;
+
+#include <openssl/ssl.h>	// Secure Socket Layer library
+#include <openssl/bio.h>	// Basic Input/Output objects for SSL
+#include <openssl/rsa.h>	// RSA algorithm etc
+#include <openssl/pem.h>	// For reading .pem files for RSA keys
+#include <openssl/err.h>	// ERR_get_error()
+#include <openssl/dh.h>		// Diffie-Helman algorithms & libraries
+
+#include "utils.h"
+
+//----------------------------------------------------------------------------
+// Function: main()
+//----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+  //-------------------------------------------------------------------------
+  // Initialization
+  
+  ERR_load_crypto_strings();
+  SSL_library_init();
+  SSL_load_error_strings();
+  
+  setbuf(stdout, NULL); // disables buffered output
+  
+  // Handle commandline arguments
+  // Useage: client server:port filename
+  if (argc < 3)
+    {
+      printf("Useage: client -server serveraddress -port portnumber filename\n");
+      exit(EXIT_FAILURE);
+    }
+  char* server = argv[1];
+  char* filename = argv[2];
+  
+  printf("------------\n");
+  printf("-- CLIENT --\n");
+  printf("------------\n");
+  
+  //-------------------------------------------------------------------------
+  // 1. Establish SSL connection to the server
+  printf("1.  Establishing SSL connection with the server...");
+  
+  // Setup client context
+  SSL_CTX* ctx = SSL_CTX_new(SSLv23_method());
+  SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+  //	SSL_CTX_set_options(ctx, SSL_OP_ALL|SSL_OP_NO_SSLv2);
+  if (SSL_CTX_set_cipher_list(ctx, "ADH") != 1)
+    {
+      printf("Error setting cipher list. Sad christmas...\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  
+  // Setup the BIO
+  BIO* client = BIO_new_connect(server);
+  if (BIO_do_connect(client) != 1)
+    {
+      printf("FAILURE.\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  
+  // Setup the SSL
+  SSL* ssl=SSL_new(ctx);
+  if (!ssl)
+    {
+      printf("Error creating new SSL object from context.\n");
+      exit(EXIT_FAILURE);
+    }
+  SSL_set_bio(ssl, client, client);
+  if (SSL_connect(ssl) <= 0)
+    {
+      printf("Error during SSL_connect(ssl).\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  
+  printf("SUCCESS.\n");
+  printf("    (Now connected to %s)\n", server);
+  
+  //-------------------------------------------------------------------------
+  // 2. Send the server a random number
+  printf("2.  Sending challenge to the server...");
+  /*
+  char randomNumber_challenge[129];
+  memset(randomNumber_challenge,0,sizeof(randomNumber_challenge));
+  int randSize = 128;
+  int created_random = RAND_bytes( (unsigned char *)randomNumber_challenge, randSize);
+  string randomNumber = randomNumber_challenge;
+  */  
+string randomNumber="31337";
+  //string randomNumber="31333333";
+  //SSL_write
+  int sent = SSL_write(ssl,(const void*) randomNumber.c_str(),sizeof(randomNumber));
+  
+  printf("SUCCESS.\n");
+  printf("    (Challenge sent: \"%s\")\n", buff2hex((const unsigned char*)randomNumber.c_str(),sizeof(randomNumber)).c_str());
+  
+  //-------------------------------------------------------------------------
+  // 3a. Receive the signed key from the server
+  printf("3a. Receiving signed key from server...");
+
+  string signature ="FIXME";
+  int siglen = 5;
+  //SSL_read;
+  {
+     char rsa_enc_buff[BUFFER_SIZE];
+     memset(rsa_enc_buff,0,sizeof(rsa_enc_buff));
+     int rsa_private_enc = SSL_read(ssl,rsa_enc_buff,BUFFER_SIZE);
+     signature = rsa_enc_buff;
+     siglen = rsa_private_enc;
+     
+  }
+  printf("RECEIVED.\n");
+  printf("    (Signature: \"%s\" (%d bytes))\n", buff2hex((const unsigned char*)signature.c_str(), siglen).c_str(), siglen);
+  //-------------------------------------------------------------------------
+  // 3b. Authenticate the signed key
+  printf("3b. Authenticating key...");
+  
+  //BIO_new(BIO_s_mem())
+  //BIO_write
+  //BIO_new_file
+  //PEM_read_bio_RSA_PUBKEY
+  //RSA_public_decrypt
+  //BIO_free
+  
+  string generated_key="";
+  int generated_length = 0;
+  string decrypted_key="";
+  int decrypted_length = 0;
+ 
+  {
+    BIO *hash, *boutfile;
+    //generating hash
+    boutfile = BIO_new(BIO_s_mem());
+    hash = BIO_new(BIO_f_md());
+    BIO_set_md(hash,EVP_sha1());
+    int actualWritten = BIO_write(boutfile,(const void*) randomNumber.c_str(),sizeof(randomNumber));
+    BIO_push(hash,boutfile);
+    char mdbuf[BUFFER_SIZE];
+    memset(mdbuf,0,sizeof(mdbuf));
+    int mdlen = BIO_read(hash,mdbuf,BUFFER_SIZE);
+    generated_key = buff2hex((const unsigned char*)mdbuf,mdlen);
+    generated_length = mdlen;
+    int hash_flush = BIO_flush(hash);
+    int boutfile_flush = BIO_flush(boutfile);
+    print_errors();
+  }
+  
+  {
+    BIO  *rsa_public;
+    RSA *RSAPUB;
+    //decrypting signature given
+    char rsa_dec_buff[BUFFER_SIZE];
+   rsa_public = BIO_new_file("rsapublickey.pem","r");
+   RSAPUB = PEM_read_bio_RSA_PUBKEY(rsa_public,NULL,NULL,NULL);
+   int rsa_public_dec = RSA_public_decrypt(siglen,(unsigned char*)signature.c_str(),(unsigned char*)rsa_dec_buff,RSAPUB,RSA_PKCS1_PADDING);
+   print_errors();
+   decrypted_key = buff2hex((const unsigned char*) rsa_dec_buff,rsa_public_dec);
+   decrypted_length = rsa_public_dec;
+   int rsa_public_flush = BIO_flush(rsa_public);
+ }
+  
+  printf("AUTHENTICATED\n");
+  printf("    (Generated key(SHA1 hash): %s\" (%d bytes))\n", generated_key.c_str(),generated_length);
+  printf("    (Decrypted key(SHA1 hash): %s\" (%d bytes))\n", decrypted_key.c_str(),decrypted_length);
+  if(generated_key != decrypted_key){
+    cerr << "Generated key is not the same as the Decrypted key." 
+	 << endl
+	 << "Authentication failure between client and server."
+	 << endl << "Exiting with error code -1";
+    exit(-1);
+  }
+  else
+    cout << "Genratred key matches Decrypted key." 
+	 << endl
+	 << "Authentication is a success proceeding with further "
+	 << "request(s)." << endl;
+
+  //-------------------------------------------------------------------------
+  // 4. Send the server a file request
+  printf("4.  Sending file request to server...");
+  
+  PAUSE(2);
+  //BIO_flush
+  //BIO_puts
+  //SSL_write
+  
+  //BIO_flush in part 3b so not needed here
+  int file_request = 0;
+  //int file_request = SSL_write(ssl,(const void*)filename,sizeof(filename));
+  BIO *request;
+  request = BIO_new(BIO_s_mem());
+  char filename_to_send[BUFFER_SIZE];
+  memset(filename_to_send,0,sizeof(filename_to_send));
+  int puts = BIO_puts(request,(const char*)filename);
+  int pushed_to_request = BIO_read(request,filename_to_send,puts);
+  //cout << "?" <<filename_to_send <<"?"<< endl;
+  file_request = SSL_write(ssl,(const void*) filename_to_send,
+			   pushed_to_request);
+  //flush BIO request
+  int flush_request = BIO_flush(request);
+  
+ print_errors();
+  printf("SENT.\n");
+  printf("    (File requested: \"%s\" (%d bytes))\n", 
+	 filename,file_request);
+  
+  //-------------------------------------------------------------------------
+  // 5. Receives and displays the contents of the file requested
+  printf("5.  Receiving response from server...");
+  
+  //BIO_new_file
+  //SSL_read
+  //BIO_write
+  //BIO_free
+  //checking to see if server has any problems with opening 
+  //requested file
+  {
+    char check_buff[BUFFER_SIZE];
+    memset(check_buff,0,sizeof(check_buff));
+    int check_error_received = SSL_read(ssl,check_buff,
+					sizeof(check_buff));
+    if(string(check_buff) == "OK"){
+      cout << endl 
+	   <<"Server is able to process file request" << endl;
+      //getting data from server
+      {
+	BIO *fileout;
+	fileout = BIO_new_file(filename,"w");
+	int flush_server = BIO_flush(client);
+	int bytesReceived = 0;
+	int bytesWrote = 0;
+	char receive_buf[1024];
+	memset(receive_buf,0,sizeof(receive_buf));
+	int readAmount = 1023;
+	int actualRead = 0;
+	int actualWritten = 0;
+	cout << endl;
+	while((actualRead = SSL_read(ssl,receive_buf,readAmount)) >1)
+	  {
+	    bytesReceived += actualRead;
+	    //print what was read from SSL_read
+	    cout << receive_buf;
+	    //finish outputting the file
+	    memset(receive_buf,0,sizeof(receive_buf));
+	  }
+	printf("\nFILE RECEIVED.\n");
+	printf("Recieved.\n");
+	printf("    (Bytes sent: %d)\n", bytesReceived);
+	printf("Wrote to file: %s\n",filename);
+	printf("    (Bytes wrote %d)\n", bytesWrote);
+	int flush_fileout = BIO_flush(fileout);
+	int free_fileout  = BIO_free(fileout);
+	int free_server_fileout = BIO_flush(client);
+      }
+    }
+    else if(string(check_buff) == "fnf"){
+      cout << endl 
+	   << "Server is having a problem sending " << filename 
+	   << "." << endl
+	   << "Associating error as file not found (fnf)." << endl;
+      //proceeding with getting data from 
+      
+    }
+    else
+      {
+	cerr << endl  
+	     << "Receiveing unknown ACK<" << check_buff << "> " 
+	     <<"from server." << endl
+	     << "Exiting program to prevent any loss of data"
+	     << " with error -1."
+	     << endl;
+	exit(-1);
+	printf("FILE NOT RECEIVED.\n");
+      }
+  }
+  
+  
+  //-------------------------------------------------------------------------
+  // 6. Close the connection
+  printf("6.  Closing the connection...");
+  
+  //SSL_shutdown
+  //1 = good
+  //0 = problem call it again
+  //-1 = error
+  int server_shutdown = SSL_shutdown(ssl);
+  print_errors();
+  printf("DONE.\n");
+  
+  printf("\n\nALL TASKS COMPLETED SUCCESSFULLY.\n");
+  
+  //-------------------------------------------------------------------------
+  // Freedom!
+  SSL_CTX_free(ctx);
+  SSL_free(ssl);
+  return EXIT_SUCCESS;
+  
+}
---
+++ project/Server/ssl_server.cpp
@@ -0,0 +1,301 @@
+//----------------------------------------------------------------------------
+// File: ssl_server.cpp
+// Description: Implementation of an SSL-secured server that performs
+//              secure file transfer to a single client over a single
+//              connection.
+//----------------------------------------------------------------------------
+#include <string>
+#include <time.h>
+//added
+#include <iostream>
+using namespace std;
+
+#include <openssl/ssl.h>	// Secure Socket Layer library
+#include <openssl/bio.h>	// Basic Input/Output objects for SSL
+#include <openssl/rsa.h>	// RSA algorithm etc
+#include <openssl/pem.h>	// For reading .pem files for RSA keys
+#include <openssl/err.h>
+
+#include "utils.h"
+
+//-----------------------------------------------------------------------------
+// Function: main()
+//-----------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    //-------------------------------------------------------------------------
+    // initialize
+  ERR_load_crypto_strings();
+  SSL_load_error_strings();
+  SSL_library_init();
+  
+  setbuf(stdout, NULL); // disables buffered output
+  
+  // Handle commandline arguments
+  // Useage: client -server serveraddress -port portnumber filename
+  if (argc < 2)
+    {
+      printf("Useage: server:portnumber\n");
+      exit(EXIT_FAILURE);
+    }
+  char* port = argv[1];
+  
+  printf("------------\n");
+  printf("-- SERVER --\n");
+  printf("------------\n");
+  
+  //-------------------------------------------------------------------------
+  // 1. Allow for a client to establish an SSL connection
+  printf("1. Allowing for client SSL connection...");
+  
+  // Setup DH object and generate Diffie-Helman Parameters
+  DH* dh = DH_generate_parameters(128, 5, NULL, NULL);
+  int dh_err;
+  DH_check(dh, &dh_err);
+  if (dh_err != 0)
+    {
+      printf("Error during Diffie-Helman parameter generation.\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  
+  // Setup server context
+  SSL_CTX* ctx = SSL_CTX_new(SSLv23_method());
+  SSL_CTX_set_verify(ctx, SSL_VERIFY_NONE, NULL);
+  //	SSL_CTX_set_options(ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_SINGLE_DH_USE);
+  SSL_CTX_set_tmp_dh(ctx, dh);
+  if (SSL_CTX_set_cipher_list(ctx, "ALL") != 1)
+    {
+      printf("Error setting cipher list. Sad christmas...\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  
+  // Setup the BIO
+  BIO* server = BIO_new(BIO_s_accept());
+  BIO_set_accept_port(server, port);
+  BIO_do_accept(server);
+  
+  // Setup the SSL
+  SSL* ssl = SSL_new(ctx);
+  if (!ssl)
+    {
+      printf("Error creating new SSL object from context.\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  SSL_set_accept_state(ssl);
+  SSL_set_bio(ssl, server, server);
+  if (SSL_accept(ssl) <= 0)
+    {
+      printf("Error doing SSL_accept(ssl).\n");
+      print_errors();
+      exit(EXIT_FAILURE);
+    }
+  
+  printf("DONE.\n");
+  printf("    (Now listening on port: %s)\n", port);
+  
+  //-------------------------------------------------------------------------
+  // 2. Receive a random number (the challenge) from the client
+  printf("2. Waiting for client to connect and send challenge...");
+  
+  //SSL_read
+  string challenge="";
+  
+  int buff_len = 0;
+  char buff[BUFFER_SIZE];
+  memset(buff,0,sizeof(buff));
+  buff_len = SSL_read(ssl,buff,BUFFER_SIZE);    
+  //challenge = buff;
+  //cout << endl<< buff << endl;
+  printf("DONE.\n");
+  printf("    (Challenge: \"%s\")\n", buff2hex((const unsigned char*)buff, buff_len).c_str() );
+  
+  //-------------------------------------------------------------------------
+  // 3. Generate the SHA1 hash of the challenge
+  printf("3. Generating SHA1 hash...");
+ 
+  //BIO_new(BIO_s_mem());
+  //BIO_write
+  //BIO_new(BIO_f_md());
+  //BIO_set_md;
+  //BIO_push;
+  //BIO_gets;
+
+ int mdlen=0;
+ string hash_string = ""; 
+ 
+ BIO *hash,*boutfile;
+ boutfile = BIO_new(BIO_s_mem());
+ hash = BIO_new(BIO_f_md());
+ BIO_set_md(hash,EVP_sha1());
+ int actualWritten = BIO_write(boutfile,buff,buff_len);  
+ BIO_push(hash,boutfile); 
+ char mdbuf[BUFFER_SIZE];
+ memset(mdbuf,0,sizeof(mdbuf));
+ mdlen = BIO_read(hash,mdbuf,BUFFER_SIZE);
+ hash_string =  buff2hex((const unsigned char*)mdbuf,mdlen);
+
+  printf("SUCCESS.\n");
+  printf("    (SHA1 hash: \"%s\" (%d bytes))\n", hash_string.c_str(), mdlen);
+  
+  //-------------------------------------------------------------------------
+  // 4. Sign the key using the RSA private key specified in the
+  //     file "rsaprivatekey.pem"
+  printf("4. Signing the key...");
+  
+  //PEM_read_bio_RSAPrivateKey
+  //RSA_private_encrypt
+  
+  int siglen=0;
+  string signature = "FIXME"; 
+  BIO *rsa_private;
+  RSA * RSAPRIV;
+  {
+    char rsa_enc_buff[BUFFER_SIZE];
+    rsa_private = BIO_new_file("rsaprivatekey.pem","r");
+    RSAPRIV = PEM_read_bio_RSAPrivateKey(rsa_private,NULL,NULL,NULL);
+    int rsa_private_enc = RSA_private_encrypt(mdlen,(unsigned char*)mdbuf,(unsigned char*)rsa_enc_buff,RSAPRIV,RSA_PKCS1_PADDING);
+    siglen = rsa_private_enc;
+    signature = rsa_enc_buff;
+    print_errors();
+  }
+  printf("DONE.\n");
+  printf("    (Signed key length: %d bytes)\n", siglen);
+  printf("    (Signature: \"%s\" (%d bytes))\n", buff2hex((const unsigned char*)signature.c_str(), siglen).c_str(), siglen);
+  
+  //-------------------------------------------------------------------------
+  // 5. Send the signature to the client for authentication
+  printf("5. Sending signature to client for authentication...");
+ 
+//BIO_flush
+  int flush_bout = BIO_flush(boutfile);
+  int flush_hash = BIO_flush(hash);
+  int flush_rsa = BIO_flush(rsa_private);
+  //SSL_write
+  int sent_signiture = SSL_write(ssl,(const void*) signature.c_str(),siglen);
+  
+  // cout << endl << "SENT(sig to client)<"<<sent_signiture <<">" <<endl;
+  
+    /*  
+  //checking signature on server side an seeing if I can decrypt
+  {  
+    cout << "CHECKING signature on server side to see if it can"
+	 << " decrypted" << endl;
+    BIO *rsa_public;
+    RSA *RSAPUB;
+    char rsa_dec_buff[BUFFER_SIZE];
+    rsa_public = BIO_new_file("rsapublickey.pem","r");
+    RSAPUB = PEM_read_bio_RSA_PUBKEY(rsa_public,NULL,NULL,NULL);
+    int rsa_public_dec = RSA_public_decrypt(siglen,(unsigned char*)signature,(unsigned char*)rsa_dec_buff,RSAPUB,RSA_PKCS1_PADDING);
+     print_errors();
+    cout << endl;
+    printf("    (Decrypted key length: %d bytes)\n", rsa_public_dec);
+    printf("    (Decrypted key: \"%s\" (%d bytes))\n", buff2hex((const unsigned char*)rsa_dec_buff, rsa_public_dec).c_str(), rsa_public_dec);
+    flush_bout = BIO_flush(boutfile);
+    flush_hash = BIO_flush(hash);
+    flush_rsa = BIO_flush(rsa_private);
+    int flush_rsa_dec = BIO_flush(rsa_public);
+  }
+  */
+
+  printf("DONE.\n");
+  
+  //-------------------------------------------------------------------------
+  // 6. Receive a filename request from the client
+  printf("6. Receiving file request from client...");
+  
+  //SSL_read
+  char file[BUFFER_SIZE];
+  memset(file,0,sizeof(file));
+  int file_len = 0;
+  {
+    file_len = SSL_read(ssl,file,BUFFER_SIZE);    
+  }
+  printf("RECEIVED.\n");
+  printf("    (File requested: \"%s\" (%d bytes))\n", file,file_len);
+  
+  //-------------------------------------------------------------------------
+  // 7. Send the requested file back to the client (if it exists)
+  printf("7. Attempting to send requested file to client...");
+  
+  PAUSE(2);
+  //BIO_flush
+  //BIO_new_file
+  //fnf = file not found
+  //BIO_puts(server, "fnf"); //
+  //BIO_read(bfile, buffer, BUFFER_SIZE)) > 0)
+  //SSL_write(ssl, buffer, bytesRead);
+  int bytesSent=0;
+  {
+    BIO *fileout;
+    fileout = BIO_new_file(file,"r");
+    int flush_server = BIO_flush(server);
+    //checking file to see if error opening it
+    {
+      string error = "OK";
+      int error_len = 0;
+      if(fileout == NULL)
+	{
+	  cout << endl 
+	       << "Not able to open file" << endl
+	       << "Associatiating error with file not found (fnf)."
+	       << endl;
+	  error = "fnf";
+	  error_len = SSL_write(ssl,error.c_str(),sizeof(error)+1);
+	}
+      else
+	{
+	cout << endl 
+	     << file << " was found proceeding with sending file" 
+	     << endl;
+	error_len = SSL_write(ssl,error.c_str(),sizeof(error)+1);
+	}
+      int flush_server = BIO_flush(server);
+    }
+    //writing contents of requested file to client
+    {
+      char send_buf[1024];
+      memset(send_buf,0,sizeof(send_buf));
+      int readAmount = 1023;
+      int actualRead = 0;
+      int actualWritten = 0;
+      while((actualRead = BIO_read(fileout,send_buf,readAmount))>1)
+	{
+	  actualWritten = SSL_write(ssl,send_buf,actualRead);
+	  bytesSent += actualWritten;
+	  
+	}
+    }
+    int flush_fileout = BIO_flush(fileout);
+    int free_fileout  = BIO_free(fileout);
+    int free_server_fileout = BIO_flush(server);
+  }
+ 
+  print_errors();
+  printf("SENT.\n");
+  printf("    (Bytes sent: %d)\n", bytesSent);
+  
+  //-------------------------------------------------------------------------
+  // 8. Close the connection
+  printf("8. Closing connection...");
+  
+  //SSL_shutdown
+  //BIO_reset
+  //1 = good
+  //0 = problem call it again
+  //-1 = error
+  int server_shutdown = SSL_shutdown(ssl);
+  int bio_server_reset = BIO_reset(server);
+  print_errors();
+  printf("DONE.\n");
+  
+  printf("\n\nALL TASKS COMPLETED SUCCESSFULLY.\n");
+  
+  //-------------------------------------------------------------------------
+  // Freedom!
+  
+  BIO_free_all(server);
+  return EXIT_SUCCESS;
+}
